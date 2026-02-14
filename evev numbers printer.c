39fea61ee67bdc919d8cf2ccd47c7582092888c6
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-02-14 22:08:02 +0530

    Add files via upload

---
+++ evev numbers printer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter the value:");
+    scanf("%d",&n);
+    for(int i=2;i<=10;i+=2){
+
+    
+    printf("%d\n",i);
+    }
+    
+    return 0;
+
+}
