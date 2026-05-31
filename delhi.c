e2f87084e4b06f9078b4e726868e7fcf782c0b88
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-05-31 22:59:41 +0530

    Add files via upload

---
+++ delhi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    char *name;
+    int length;
+    char *cptr=name;
+    name="DELHI";
+    printf("%s\n",name);
+    while(*cptr!='\0')
+    {
+        printf("%c is stored at address %u\n",*cptr,cptr);
+    }
+    length=cptr-name;
+    printf("\nLength of the string -%d\n",length);
+    return 0;
+}
