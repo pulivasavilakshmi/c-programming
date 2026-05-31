7ad3362000f2fd2c9cab3ca119567a0819806e4f
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-05-31 22:58:49 +0530

    Add files via upload

---
+++ pointers increment.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	int *p1,*p2;
+	char c,d;
+	char *p3,*p4;
+	float e,f;
+	float *p5,*p6;
+	printf("The address of a % v and b%v",&a,&b);
+	/* Now the address of a and b is*/
+	/* Incrementing pointers*/
+	p1++;
+	p2++;
+	/* Now the address of a and b is*/
+}
