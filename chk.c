d834d6a268c23d4a223467dd651d93ae19196909
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-06-02 23:46:46 +0530

    Add files via upload

---
+++ chk.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char chk='t',str[30];
+	int len,left,right;
+	printf("\nEnter the string:");
+	scanf("%s",str);
+	len=strlen(str);
+	left=0;
+	right=len-1;
+	while(left<right &&chk=='t')
+	{
+		if(!(str[left]==str[right]))
+              chk='f';
+              left++;
+              right--;
+              
+	}
+	if(chk=='t')
+	printf("\nThe string %s is a palindrome",str);
+	else
+	printf("\nThe string %s is not a palindrome",str);
+}
