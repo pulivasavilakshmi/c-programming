e38c77c0b0d56fdab14cc0e898d9fd7210ff3810
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-06-14 22:02:06 +0530

    Add files via upload

---
+++ star 13.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char i,j,n;
+    scanf("%c",&n);
+    for (i='A';i<=n;i++) {
+        for (j='A';j<i+1;j++) {
+            printf("%c ",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
