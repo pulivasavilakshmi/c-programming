884ed8114f38fa76524630a7275bdb9b3e9682fe
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-06-18 21:45:09 +0530

    Add files via upload

---
+++ int a,b.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+int A,B,Answer;
+printf("ENTER THE VALUE FOR A:\n");
+printf("Hello,My friend you Entered %d:\n",A);
+scanf("%d",&A,&B);
+printf("...................");
+printf("ENTER THE VALUE FOR B:\n");
+printf("Hello,My friend you Entered %d:\n",B);
+}
