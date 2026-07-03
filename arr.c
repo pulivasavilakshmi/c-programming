0392ff50881bcb745a81d14f4815177ca730185f
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-07-03 22:38:34 +0530

    Add files via upload

---
+++ arr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() 
+int *num,*freq,i,size;
+printf("\n enter the size of the list:");
+scanf("%d",&size);
+num=(int*)malloc(size*sizeof(int));
+printf("\n Enter the numbers:");
+for(i=0;i<size;i++)
+{
+
+    printf("\n enter num[%d]:",i);
+    scanf("%d",&num[i]);
+{
+if(num[i]<0||num[i]>4){
+    printf("\nNumber should be within range(0-4)");
+    i--;
+    continue;
+ }
+}
+freq=(int*)calloc(5,sizeof(int));
+for(i=0;i<size;i++)
+freq[num[i]]++;
+printf("\nThe frequencies of the numbers are:");
+for(i=0;i<5;i++)
+printf("\nfreq[%d]=%d",i,freq[i]);
+}
+
