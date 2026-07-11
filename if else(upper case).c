0c6c67f8e40c931604b51681244c41b31003e2cf
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-07-11 21:40:36 +0530

    Add files via upload

---
+++ if else(upper case).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+      char ch ;
+      printf("enter ch = ");
+      scanf("%c",& ch);
+      if(ch >='A' && ch <= 'Z'){
+        printf("Upper case");
+      }
+      else if(ch >= 'a' && ch <= 'z'){
+        printf("Lower case");
+      }
+      else{
+        printf("invalid case");
+      }
+    
+    return 0;
+}
