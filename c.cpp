6fdef35c180129c8e99522c2d85a50f62623fc33
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-07-12 21:52:41 +0530

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello How are you?\r");
+    printf("My name is vasavi\r");
+    printf("Today is 19th May 2026\r");
+    printf("I am at sscet sangagiri\r");
+    printf("students are learning c coding\r");
+    printf("For some time there was an power\r");
+    printf("Now power is okay\r");
+    printf("I am a student\r");
+    printf("My favourite colour black\r");
+    printf("I am intersted in software developer\r");
+    printf("My sister name is Moulimanasa\r");
+    printf("I am good in c programming\r");
+    printf("I know about my self\r");
+    printf("Today is not good\r");
+    printf("I am sleepy\r");
+    printf("In last sunday i am very happy\r");
+    printf("All is well\r");
+    printf("I have trining for 13th days\r");
+    printf("My hobbies  listening music\r");
+    printf("I am a good girl\r");
+    printf("I lerning new things\r");
+    printf("What about your self?\r");
+    printf("I love my family\r");
+    printf("My favourite food is briyani\r");
+}
+
