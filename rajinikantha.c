48828c9b8340d5a6d5bd3d4b82438c1a5df173d4
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-07-29 13:25:29 +0530

    Add files via upload

---
+++ rajinikantha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<string.h>
+int main(){
+char name[]="Rajini kanth";
+n=strlen(name);
+
+char cinema[]="super star";
+printf("%s has %d Letters",name,n);
+printf("%s",strcat(name,cinema));
+
+
+
+}  
