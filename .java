7ec3afbd0436a9be555400c8b6194531e39f4671
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-07-31 11:42:43 +0530

    Add files via upload

---
+++ .java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        // Your Code goes here!
+import java.util.Scanner;
+
+    public static void main(String[] args) {
+        String name = " ", pan = " ",email = " ",aadhaar = " ", mobile = " ";
+        boolean isValid = true;
+        Scanner sscet = new Scanner(System.in);
+        System.out.println("enter the name");
+        name = sscet.nextLine();
+        System.out.println("enter the email");
+        email = sscet.next();
+        System.out.println("enter the mobile number");
+        mobile = sscet.next();
+        System.out.println("enter aadhaar");
+        aadhaar = sscet.next();
+        System.out.println("enter PAN");
+        pan = sscet.next();
+        if(!name.matches("^[A-Za-z ]{5,}$"))
+        {isValid=false;System.out.println("Invalid Name");}
+        if(!email.matches("^[A-Za-z0-9_.-]{2,}@[A-Za-z]{3,}+\\.[a-z]{2,}$"))
+        {isValid=false;System.out.println("Invalid email");}
+        if(!mobile.matches("^[0-9]{10}$"))
+            {isValid=false;System.out.println("Invalid  mobile");}
+        if(!aadhaar.matches("^[0-9]{12}$"))
+            {isValid=false;System.out.println("Invalid  aadhaar");}
+          if(!pan.matches("^[A-Z{5}[0-9]{4}[A-Z]{1}$"))
+            {isValid=false;System.out.println("Invalid  PAN");}
+         if(isValid) 
+            System.out.println(name+"voter application submitted further updates send to"+mobile+"or"+email);
+        else System.out.println("Application Rejected");
+
+
+    }
+    
+    
+}
+    }
