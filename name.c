5aedbc7f07ea6f0fde3f36ae1bfb9ca16aad5426
pulivasavilakshmi <pulivasavilakshmi@gmail.com>
2026-09-18 22:43:16 +0530

    Add files via upload

---
+++ name.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<string.h>
+#include<type.h>
+int main()
+{
+char name[20]="vasavi lakshmi"
+printf("The length of my name is %d",strlen(name1));
+char name[20]="gowthami"
+printf("The length of my name is %d",strlen(name2));
+char name[20]="pushpa"
+printf("The length of my name is %d",strlen(name2));
+char name[20]="divya"
+printf("The length of my name is %d",strlen(name3));
+char name[20]="manasa"
+printf("The length of my name is %d",strlen(name4));
+char name[20]="sarath"
+printf("The length of my name is %d",strlen(name5));
+char name[20]="parvathi"
+printf("The length of my name is %d",strlen(name6));
+char name[20]="praveen"
+printf("The length of my name is %d",strlen(name7));
+char name[20]="janani"
+printf("The length of my name is %d",strlen(name8));
+char name[20]="naresh"
+printf("The length of my name is %d",strlen(name9));
+char name[20]="manoj"
+printf("The length of my name is %d",strlen(name10));
+char name[20]="sravanthi"
+printf("The length of my name is %d",strlen(name11));
+char name[20]="rakshitha"
+printf("The length of my name is %d",strlen(name12));
+char name[20]="nagaraju"
+printf("The length of my name is %d",strlen(name13));
+char name[20]="bhumi"
+printf("The length of my name is %d",strlen(name14));
+char name[20]="ram"
+printf("The length of my name is %d",strlen(name15));
+char name[20]="lokesh"
+printf("The length of my name is %d",strlen(name16));
+char name[20]="likitha"
+printf("The length of my name is %d",strlen(name17));
+char name[20]="pallavi"
+printf("The length of my name is %d",strlen(name18));
+char name[20]="pallavi"
+printf("The length of my name is %d",strlen(name19));
+char name[20]="mahi"
+printf("The length of my name is %d",strlen(name20));
+char name[20]="esther"
+}
